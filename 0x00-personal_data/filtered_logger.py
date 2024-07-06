e442d99b432ca0b53c0fd31c4675af9b94e85adc
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-06 21:07:32 +0000

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,7 @@ import logging
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
