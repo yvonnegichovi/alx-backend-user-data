4507c7240a8dde762e594dc7717643cd6ee45cc2
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-06 22:32:12 +0000

    task 6

--- 0x00-personal_data/main.sql
+++ 0x00-personal_data/main.sql
@@ -2,14 +2,18 @@
 -- configure permissions
 CREATE DATABASE IF NOT EXISTS my_db;
 CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON my_db.* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON my_db.* TO root@localhost;
 
 USE my_db;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    email VARCHAR(256)
+    name VARCHAR(256), 
+        email VARCHAR(256), 
+        phone VARCHAR(16),
+    ssn VARCHAR(16), 
+        password VARCHAR(256),
+    ip VARCHAR(64), 
+        last_login TIMESTAMP,
+    user_agent VARCHAR(512)
 );
-
-INSERT INTO users(email) VALUES ("bob@dylan.com");
-INSERT INTO users(email) VALUES ("bib@dylan.com");
