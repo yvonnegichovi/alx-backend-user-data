13dc0ac4229baea50c323940f375415bab7d7954
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-10 01:56:23 +0300

    3 basic authenticatation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,6 +17,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 AUTH_TYPE = os.getenv('AUTH_TYPE', 'auth')
+if AUTH_TYPE = 'auth':
+    auth = Auth()
+
+if AUTH_TYPE = 'basic_auth':
+    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -40,14 +45,6 @@ def forbidden(error) -> str:
     return jsonify({
                     "error": "Forbidden"
                   }), 403
-
-
-if AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
-    from  api.v1.auth.auth import Auth
-    auth = Auth()
 
 
 @app.before_request
