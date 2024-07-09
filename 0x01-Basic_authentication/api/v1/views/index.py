f3f97728d848a803c0a77a0bc9136bbfbc3556e9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-10 00:14:09 +0300

    forbidden error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unathorized() -> str:
       - the unathorized error of the API
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - the forbidden error of the API
+    """
+    abort(403)
