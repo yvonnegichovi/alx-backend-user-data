eac51bf4420d718bbf8f619d8d9dbd0299f5e01c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-10 21:52:01 +0300

    retrieves a user based on the user authentication credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,3 +58,24 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on his email and password
+        """
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+        user_list = User.search({'email': user_email})
+        if not user_list:
+            return None
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        else:
+            return user
+        return User
