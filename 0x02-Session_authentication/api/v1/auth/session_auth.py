d73f3a8e16ea4bc02bb6b4a676d2f8b845befecb
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-12 00:20:42 +0300

    task 3.1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,5 +24,5 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] == user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
