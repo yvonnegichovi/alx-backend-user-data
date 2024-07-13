1f0098f67a978df90087b6b7d5f9c8018239e9d6
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-14 02:21:31 +0300

    import error fixed7

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
@@ -30,49 +30,47 @@ if auth_type == 'session_exp_auth':
 if auth_type == 'session_db_auth':
     auth = SessionDBAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
+    """Unauthorized handler.
     """
-    return jsonify({
-                    "error": "Unauthorized"
-                  }), 401
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler
+    """Forbidden handler.
     """
-    return jsonify({
-                    "error": "Forbidden"
-                  }), 403
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request():
-    """Before request hook to handle authentication."""
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-    ]
-    if auth and auth.require_auth(request.path, excluded_paths):
-        auth_header = auth.authorization_header(request)
-        auth_session = auth.session_cookie(request)
-        if not auth_header and not auth_session:
-            abort(401)
-        user = auth.current_user(request)
-        if not user:
-            abort(403)
-        request.current_user = user
+def authenticate_user():
+    """Authenticates a user before processing a request.
+    """
+    if auth:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
