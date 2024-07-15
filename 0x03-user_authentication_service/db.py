a4699e3175e6d20b1ca3cc8f9928f9980fd0d926
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-15 22:54:46 +0300

    auth

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
+import bycrypt
 
 
 class DB:
@@ -38,3 +41,53 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Finds a user by arbitrary keyword arguments
+        Args:
+            - **kwargs: Arbitrary keyword arguments for filtering the query
+        Returns:
+            - User: The first user found
+        Raises:
+            - NoResultFound: If no user is found
+            - InvalidRequestError: If the query arguments are invalid
+        """
+        try:
+            query = self._session.query(User).filter_by(**kwargs)
+            user = query.one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the provided arguments.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments provided.")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Updates a user
+        Args:
+            - user_id: user id to be updated
+            - **kwargs: arbitrary keyword arguments
+        Returns:
+            - None
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError("Invalid argument: {}".format(key))
+        self._session.commit()
+
+    @property
+    def _hashpassword(self, password: str) -> bytes:
+        """
+        Defines passwords
+        Args:
+            - password: password
+        returns:
+            - bytes: salted hash of the input password
+        """
+        salt = bycrypt.gensalt()
+        hashed_password = bycrypt.hashpwd(password.encode('utf-8'), salt)
+        return hashed_password
