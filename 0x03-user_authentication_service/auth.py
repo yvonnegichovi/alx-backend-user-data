b087105055295143467d64b24f2328e3678e31ae
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-16 23:19:42 +0300

    validating credentials of users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,7 +33,7 @@ class Auth:
             user = self._db.add_user(email, hashed_password)
             return user
 
-    def valid_login(self, email, password) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """
         Validates credentials
         """
