f31a001238a9bbf5fd85ab7a9c96fb736282953c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-18 00:28:56 +0300

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,8 +74,9 @@ def profile():
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        return jsonify({"email": user.email}), 200
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
