fde5053999f1a7ab5381571f490b7609bc44fe2c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-18 00:26:52 +0300

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,15 +60,22 @@ def logout():
     Routes handles logout
     """
     session_id = request.cookies.get('session_id')
-    if not session_id:
-        return jsonify({"error": "Forbidden"}), 403
-    user = None
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    except NoResultFound:
-        return jsonify({"error": "Forbidden"}), 403
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """
+    Implements user profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
