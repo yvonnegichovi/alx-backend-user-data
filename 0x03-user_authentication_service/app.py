a2309059ccc597fb6a14ccea0b30dcfe7cdda959
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-18 13:41:46 +0300

    generate reset password token route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,10 +73,22 @@ def profile():
     Implements user profile
     """
     session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id=session_id)
     if user is None:
         abort(403)
     return jsonify({"email": user.email}), 200
+
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """
+    Route implements get reset password token
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+    user = AUTH.find_user_by(email=email)
+    return ({"email": user.email, "reset_token": user.reset_token})
 
 
 if __name__ == "__main__":
